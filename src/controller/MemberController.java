aa6f8a2990306395d88b3dd8d22e9707034c2cfd
odoria <31239288+odoria@users.noreply.github.com>
2017-11-24 17:04:43 +0900

    dd

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,8 +1,12 @@
 package controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -71,5 +75,46 @@ public class MemberController {
 	@RequestMapping("/moveSignupForm.do")
 	public String moveSignupForm() {
 		return "signup_form";
+	}
+	
+	@RequestMapping("/signup.do")
+	public String join(HttpServletRequest request) {
+		MemberVO member = new MemberVO();
+		member.setId(request.getParameter("id"));
+		member.setPassword(request.getParameter("password"));
+		member.setName(request.getParameter("name"));
+		member.setEmail("email");
+		String from = request.getParameter("birth_year")+"-"+request.getParameter("birth_month")+"-"+request.getParameter("birth_day");
+		SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date to = transFormat.parse(from);
+			member.setBirth(to);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String phone = request.getParameter("phone_1")+"-"+request.getParameter("phone_2")+"-"+request.getParameter("phone_3");
+		member.setPhone(phone);
+		member.setGender(request.getParameter("gender").charAt(0));
+		if(service.signup(member)) return "login";
+		else return "join_fail";
+	}
+	
+	@RequestMapping("/overlab-check.do")
+	public void CheckOverlabID(String id, HttpServletResponse response) {
+		System.out.println(id);
+		if(service.checkOverlabID(id)) {
+		    try {
+				response.getWriter().print("true");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+		    try {
+				response.getWriter().print("false");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
