4e112d9c673d63409f2ae0a8c2eb186842f2818a
hongjunwan <dhks223@naver.com>
2017-11-28 09:48:57 +0900

    member vo 자동정렬

--- src/vo/MemberVO.java
+++ src/vo/MemberVO.java
@@ -13,63 +13,85 @@ public class MemberVO {
 	private String phone;
 	private Character gender;
 	private int follow_count;
+	
+	/////////////////////////////////////////////////////////////
+
 	public int getMember_num() {
 		return member_num;
 	}
+
 	public void setMember_num(int member_num) {
 		this.member_num = member_num;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getBirth() {
 		return birth;
 	}
+
 	public void setBirth(Date birth) {
 		this.birth = birth;
 	}
+
 	public String getPhoto_path() {
 		return photo_path;
 	}
+
 	public void setPhoto_path(String photo_path) {
 		this.photo_path = photo_path;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public Character getGender() {
 		return gender;
 	}
+
 	public void setGender(Character gender) {
 		this.gender = gender;
 	}
+
 	public int getFollow_count() {
 		return follow_count;
 	}
+
 	public void setFollow_count(int follow_count) {
 		this.follow_count = follow_count;
 	}
